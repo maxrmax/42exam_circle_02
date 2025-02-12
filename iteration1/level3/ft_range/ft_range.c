21f90b3bc5760b1aa81a259d7bc2031700f9763f
maxrmax <ringmaximilian@protonmail.com>
2025-02-12 18:35:49 +0100

    fully merged

--- iteration1/level3/ft_range/ft_range.c
+++ iteration1/level3/ft_range/ft_range.c
@@ -8,7 +8,7 @@
 // int     *ft_range(int start, int end);
 
 // It must allocate (with malloc()) an array of integers,
-	fill it with consecutive
+// fill it with consecutive
 // values that begin at start and end at end (Including start and end !), then
 // return a pointer to the first value of the array.
 
